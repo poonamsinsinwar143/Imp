3931406c29774306eacf68a77ed93db3fadb7562
poonamsinsinwar143 <poonamsinsinwar143@gmail.com>
2023-04-06 00:13:48 +0530

    Exception handling corrected

--- insurance-management-platform/src/main/java/com/imp/exception/ImpResponseEntityExceptionHandler.java
+++ insurance-management-platform/src/main/java/com/imp/exception/ImpResponseEntityExceptionHandler.java
@@ -5,17 +5,15 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ImpResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+public class ImpResponseEntityExceptionHandler {
 	private static final Logger LOG = LoggerFactory.getLogger(ImpResponseEntityExceptionHandler.class);
 	
 	@ExceptionHandler(value = { ImpBusinessException.class })
@@ -24,15 +22,19 @@ public class ImpResponseEntityExceptionHandler extends ResponseEntityExceptionHa
 		return new ResponseEntity<>(ex.getMessage(), ex.getHttpStatus());
 	}
 
-	@Override
-	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
-			HttpStatus status, WebRequest request) {
+	@ExceptionHandler(value = { MethodArgumentNotValidException.class })
+	protected ResponseEntity<Object> handleException(MethodArgumentNotValidException ex, WebRequest request) {
 		LOG.error(ex.getMessage(), ex);
 		Map<String, String> errors = new HashMap<>();
 
 		ex.getBindingResult().getFieldErrors()
 				.forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-
 		return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(value = { Exception.class })
+	protected ResponseEntity<Object> handleException(Exception ex, WebRequest request) {
+		LOG.error(ex.getMessage(), ex);
+		return new ResponseEntity<>("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
