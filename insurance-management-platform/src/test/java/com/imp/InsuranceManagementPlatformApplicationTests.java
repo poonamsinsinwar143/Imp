2efcb4f85c8bbc984957a102795a3022ab22282e
poonamsinsinwar143 <poonamsinsinwar143@gmail.com>
2023-04-05 21:23:01 +0530

    removed test class

--- insurance-management-platform/src/test/java/com/imp/InsuranceManagementPlatformApplicationTests.java
+++ insurance-management-platform/src/test/java/com/imp/InsuranceManagementPlatformApplicationTests.java
@@ -1,13 +1,13 @@
-package com.imp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class InsuranceManagementPlatformApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.imp;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class InsuranceManagementPlatformApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
